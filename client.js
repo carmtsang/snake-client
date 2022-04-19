d12695bd4c9190225693272312f7756ffa76781f
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-19 23:36:15 +0000

    set up stdin by creating setInput and handleUserInput functions

--- client.js
+++ client.js
@@ -10,19 +10,36 @@ const connect = () => {
   
   conn.on('connect', () => {
     console.log('Successfully connected to the game server')
-    conn.write('Name: CBT')
+    conn.write('Name: CBT');
+  });
+
+  conn.on('connect', () => {
+    setTimeout(() => {
+      conn.write('Move: up')
+      setTimeout(() => {
+        conn.write('Move: left')
+      }, 50);
+    }, 50);
+  });
+
+  conn.on('connect', () => {
+    setTimeout(() => {
+      conn.write('Move: up')
+      setTimeout(() => {
+        conn.write('Move: left')
+      }, 50);
+    }, 50);
   });
 
   conn.on('data', (data) => {
     console.log(data);
   });
 
-  
   // interpret incoming data as text
   conn.setEncoding('utf8');
   
   return conn;
-}
+};
 
 
 module.exports = {
