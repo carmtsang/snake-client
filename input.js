842f72775c7a3d9d675f175d9a35101216b40539
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-20 02:56:50 +0000

    updated code to include connection. directions log onto the terminal

--- input.js
+++ input.js
@@ -1,20 +1,29 @@
 let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
   
-  stdin.on('data', handleUserInput)
+  stdin.on('data', handleUserInput);
   
 
   return stdin;
 };
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
+const handleUserInput = key => {
+  if (key === 'w') {
+    console.log("Move: up");
+  } else if (key === 'a') {
+    console.log("Move: left");
+  } else if (key === 's') {
+    console.log("Move: down");
+  } else if (key === 'd') {
+    console.log("Move: right");
+  } else if (key === '\u0003') {
     process.exit(); //exit out of game if ctl C is pressed
   }
 };
